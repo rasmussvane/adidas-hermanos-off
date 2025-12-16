94084b3e9c372feb114a46796dfca0f936445b91
rasmussvane <rasmus@zetland.dk>
2025-12-16 07:42:33 +0100

    BackgroundTexture: Poster added

--- app/components/BackgroundTexture.tsx
+++ app/components/BackgroundTexture.tsx
@@ -1,7 +1,13 @@
 export default function BackgroundTexture() {
   return (
     <div className="fixed top-0 left-0 w-full h-full overflow-hidden  -z-10">
-      <video autoPlay muted loop className="w-full h-full object-cover">
+      <video
+        autoPlay
+        muted
+        loop
+        className="w-full h-full object-cover"
+        poster="/video-texture-compressed-poster.png"
+      >
         <source src="/video-texture-compressed.mp4" type="video/mp4" />
       </video>
     </div>
