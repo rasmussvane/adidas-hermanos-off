51008a9592f8b6a5d2fcd5c057ba4d151da064e8
Rasmus Svane Jensen <rasmussvane@protonmail.com>
2026-01-06 20:41:52 +0100

    Install classnames

--- app/components/SectionLayout.tsx
+++ app/components/SectionLayout.tsx
@@ -1,3 +1,5 @@
+import classNames from 'classnames';
+
 type Props = {
   children: React.ReactNode;
   className?: string;
@@ -6,7 +8,7 @@ type Props = {
 export default function SectionLayout({ children, className = "" }: Props) {
   return (
     <div
-      className={` flex flex-col justify-center items-center min-h-screen w-full p-4 overflow-hidden ${className}`}
+      className={classNames('flex flex-col justify-center items-center min-h-screen w-full p-4 overflow-hidden', className)}
     >
       {children}
     </div>
