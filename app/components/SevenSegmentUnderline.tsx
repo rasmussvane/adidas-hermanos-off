b44a8db10a0c8422a26122b0d7a8207984e42834
rasmussvane <rasmus@zetland.dk>
2026-01-14 11:43:58 +0100

    Fix input "hit box"

--- app/components/SevenSegmentUnderline.tsx
+++ app/components/SevenSegmentUnderline.tsx
@@ -1,6 +1,9 @@
 export default function SevenSegmentUnderline() {
   return (
-    <span className="font-seven-segments absolute -bottom-2 left-0 w-full select-none">
+    <span
+      className="font-seven-segments absolute -bottom-2 left-0 w-full select-none"
+      style={{ zIndex: -10 }}
+    >
       {Array.from({ length: 12 }).map((_, i) => (
         <span key={i}>_</span>
       ))}
