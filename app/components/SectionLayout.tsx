77d00b37b284bbfdbef11627c1e671a4b5259e13
Rasmus Svane Jensen <rasmussvane@protonmail.com>
2026-01-15 20:33:37 +0100

    SectionLayout: Add padding

--- app/components/SectionLayout.tsx
+++ app/components/SectionLayout.tsx
@@ -11,7 +11,7 @@ export default function SectionLayout({ children, className = '', id }: Props) {
     <div
       id={id}
       className={classNames(
-        'flex flex-col justify-center items-center min-h-dvh w-full px-sm md:px-base py-18.75 md:py-37.5',
+        'flex flex-col justify-center items-center min-h-dvh w-full px-sm md:px-base py-[150px]',
         className
       )}
     >
